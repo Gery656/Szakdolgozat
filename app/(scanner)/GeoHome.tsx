a3094a872ef4f013bf09699e9ffdc5fb7ddc048a
Gery656 <gergely.ilcsik@gmail.com>
2026-02-09 11:24:35 +0100

    Working Android geolocation

--- app/(scanner)/GeoHome.tsx
+++ app/(scanner)/GeoHome.tsx
@@ -1,11 +1,41 @@
 import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import * as Location from 'expo-location';
 
 
 export default function GeoHome() {
 
   const isFocused = useIsFocused();
+
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   
   return (isFocused &&
       <SafeAreaView
@@ -14,8 +44,9 @@ export default function GeoHome() {
         }
       > 
         <Text className='text-red-500 text-lg mt-20 mx-auto'>Geo location</Text>
+        <Text className='text-white text-lg mt-36 mx-auto'>{text}</Text>
 
-        
+
       </SafeAreaView>
   );
 }
