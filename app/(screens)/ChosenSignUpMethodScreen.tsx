6d8b35d832d61bd24df51e0ae220b39809ed3630
Gery656 <gergely.ilcsik@gmail.com>
2026-04-17 20:01:32 +0200

    Sign up with code + location works

--- app/(screens)/ChosenSignUpMethodScreen.tsx
+++ app/(screens)/ChosenSignUpMethodScreen.tsx
@@ -3,19 +3,17 @@ import SignUpBluetoothSignalsSubmitForm from "@/components/forms/signUpBluetooth
 import SignUpCodeSubmitForm from "@/components/forms/signUpCodeSubmitForm";
 import PageTitle from "@/components/ui/pageTitle";
 import { getMethodToSignUp } from "@/redux/applicationSlice";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { useSelector } from "react-redux";
 
 export default function ChosenSignUpMethodScreen(){
 
-    const {method,isGPSNeeded} = useSelector(getMethodToSignUp);
+    const {method} = useSelector(getMethodToSignUp);
 
     return(
         <View className="min-w-full min-h-full">
             
             <PageTitle title="Jelentkezés" backButton={true}></PageTitle>
-            <Text>{method}</Text>
-            <Text>{isGPSNeeded ? "true": "false"}</Text>
 
             {method==="code" && <SignUpCodeSubmitForm />}
             {/* {mode==="qr" && <SignUpQrCodeSubmitForm />} */}
--- components/forms/signUpCodeSubmitForm.tsx
+++ components/forms/signUpCodeSubmitForm.tsx
@@ -1,15 +1,141 @@
-import { Pressable, Text, TextInput, View } from "react-native";
+import { apiURL, getMethodToSignUp, getToken, setMandatoryCatalogs } from "@/redux/applicationSlice";
+import * as Location from 'expo-location';
+import { router } from "expo-router";
+import { useState } from "react";
+import { ActivityIndicator, Pressable, Text, TextInput, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignUpCodeSubmitForm() {
-    return (
+
+    const { isGPSNeeded } = useSelector(getMethodToSignUp);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [errorText, setErrorText] = useState<string[]>([]);
+    const [code, setCode] = useState("");
+    const token = useSelector(getToken);
+    const dispatch = useDispatch();
+
+    async function getCurrentLocation() {
+
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorText(["Helymeghatározás engedélyek hiányoznak!"]);
+            return null;
+        }
+
+        return await Location.getCurrentPositionAsync({});
+    }
+
+    async function SignUpOnPress() {
+        setIsLoading(true)
+
+        let location = null;
+        if (isGPSNeeded) {
+            try {
+                location = await getCurrentLocation();
+            } catch (error) {
+                setErrorText(["Helymeghatározás ki van kapcsolva!"]);
+                setIsLoading(false);
+                return
+            }
+
+            if (!location) {
+                setIsLoading(false);
+                return
+            }
+        }
+
+        const response = await fetch(apiURL + "/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+            },
+            body: JSON.stringify({
+                code: code,
+                ...(location ? { latitude: location.coords.latitude, longitude: location.coords.longitude } : {})
+            })
+        });
+
+        const recievedData = await response.json();
+
+        if (!response.ok) {
+            var errors: string[] = []
+            if (response.status === 422) {
+                if (recievedData.errors.code) {
+                    errors = [...errors, ...recievedData.errors.code]
+                }
+                if (recievedData.errors.latitude) {
+                    errors = [...errors, ...recievedData.errors.latitude]
+                }
+                if (recievedData.errors.longitude) {
+                    errors = [...errors, ...recievedData.errors.longitude]
+                }
+                if (recievedData.errors.location) {
+                    errors = [...errors, ...recievedData.errors.location]
+                }
+            }
+
+            setErrorText(errors);
+
+            if (response.status === 401) {
+                if (router.canDismiss()) {
+                    router.dismissAll()
+                }
+                router.dismissTo('/');
+            }
+            setIsLoading(false)
+            return;
+        }
+
+        const response2 = await fetch(apiURL + "/user/mandatory/catalogs", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
+            }
+        });
+
+        if (!response2.ok) {
+            if (response2.status === 401) {
+                if (router.canDismiss()) {
+                    router.dismissAll()
+                }
+                router.dismissTo('/');
+            }
+
+            setIsLoading(false);
+            return;
+        }
+
+        const recievedData2 = await response2.json();
+        dispatch(setMandatoryCatalogs(recievedData2));
+
+        setIsSuccess(true)
+        setIsLoading(false)
+    };
+
+    return (!isSuccess &&
         <View className="bg-custom-primary mt-32 w-11/12 py-3 px-3 rounded-2xl mx-auto grid grid-flow-row">
 
             <Text className="text-lg">Kód:</Text>
-            <TextInput className="border border-custom-secondary rounded-lg text-black text-lg h-12 bg-custom-background" />
+            <TextInput className="border border-custom-secondary rounded-lg text-black text-lg h-12 bg-custom-background"
+                onChange={(event) => { setCode(event.nativeEvent.text) }}
+            />
+            {errorText.length !== 0 && errorText.map((error, i) => <Text key={i} className="text-red-500">{error}</Text>)}
 
-            <Pressable className="w-full h-16 bg-custom-secondary mt-10 rounded-lg">
-                <Text className="text-[#F5F5F5] m-auto">Jelentkezés</Text>
-            </Pressable>
+            {isLoading ?
+                <View className="w-full h-16 bg-custom-secondary mt-10 rounded-lg">
+                    <ActivityIndicator className="m-auto" size={"small"}></ActivityIndicator>
+                </View>
+                :
+                <Pressable className="w-full h-16 bg-custom-secondary mt-10 rounded-lg"
+                    onPress={SignUpOnPress}>
+                    <Text className="text-[#F5F5F5] m-auto">Jelentkezés</Text>
+                </Pressable>
+            }
         </View>
     )
 }
