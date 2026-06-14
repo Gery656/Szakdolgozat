d9f7d224a78f48b0f46a0c315b1682872fdd489d
Gery656 <gergely.ilcsik@gmail.com>
2026-06-14 11:49:03 +0200

    Added a link to Icon8 to the profile page

--- app/(screens)/Profile.tsx
+++ app/(screens)/Profile.tsx
@@ -1,18 +1,26 @@
 import LogoutButton from "@/components/ui/logoutButton";
 import PageTitle from "@/components/ui/pageTitle";
 import { getUser } from "@/redux/applicationSlice";
-import { Text, View } from "react-native";
+import { Linking, ScrollView, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
 
 export default function Profile(){
     const user = useSelector(getUser);
     return(
-        <View className="min-w-full min-h-full">
-            <PageTitle title="Profil" backButton={true}></PageTitle>
-            <Text className="mx-auto text-2xl mt-10">{user.name}</Text>
-            <Text className="mx-auto text-2xl mt-2">{user.email}</Text>
-            <Text className="mx-auto text-2xl mt-2">{user.identifier}</Text>
-            <LogoutButton></LogoutButton>
-        </View>
+        <SafeAreaView className="min-w-full min-h-full">
+            <ScrollView className="min-w-full min-h-full bg-custom-background">
+                <PageTitle title="Profil" backButton={true} lowerTopMargin></PageTitle>
+                <Text className="mx-auto text-2xl mt-32">{user.name}</Text>
+                <Text className="mx-auto text-2xl mt-2">{user.email}</Text>
+                <Text className="mx-auto text-2xl mt-2">{user.identifier}</Text>
+                <LogoutButton></LogoutButton>
+                <TouchableOpacity
+                    onPress={()=>{Linking.openURL('https://icons8.com/')}}
+                    className="mx-auto mt-20">
+                    <Text className="underline text-blue-600">Minden ikon az Icon8-tól származik</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
